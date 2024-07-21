89baa4b0e6ac842a0659e4861426d95c496fbf11
runoob09 <2499469495@qq.com>
2024-07-21 10:28:24 +0800

    项目初始化

---
+++ src/main/java/github/runoob09/usercenter/UserCenterProjectApplication.java
@@ -0,0 +1,13 @@
+package github.runoob09.usercenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserCenterProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserCenterProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/github/runoob09/usercenter/UserCenterProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package github.runoob09.usercenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserCenterProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
